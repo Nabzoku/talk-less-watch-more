2db6ac6032c38a397eb4b44d86fde04640905747
Batuhan Pınar <109989430+Nabzoku@users.noreply.github.com>
2023-05-19 01:54:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const eyeInputs = document.querySelectorAll('.eye-input');
+const eyes = document.querySelector('.eyes');
+const mouthInput = document.querySelector('.mouth-input');
+const mouth = document.querySelector('.mouth');
+const text = document.querySelector('.text');
+function checkForText() {
+  text.style.color = '#ffebcd00';
+
+  setTimeout(() => {
+    text.textContent = mouth.classList.contains('switched')
+      ? 'Talk Less'
+      : 'Watch More';
+    text.style.color = '#ffebcd';
+  }, 200);
+}
+eyeInputs.forEach((eye) => {
+  eye.addEventListener('change', () => {
+    if (eye.checked) {
+      eyes.classList.toggle('switched');
+      mouth.classList.toggle('switched');
+      checkForText();
+    }
+  });
+});
+mouthInput.addEventListener('change', () => {
+  if (mouthInput.checked) {
+    mouth.classList.toggle('switched');
+    eyes.classList.toggle('switched');
+    checkForText();
+  }
+});
